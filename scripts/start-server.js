079ee834f025c848fabefa88dbebe7cc816c7e6d
amsul <reach@amsul.ca>
2015-03-04 03:03:49 -0500

    added sass middleware

--- scripts/start-server.js
+++ scripts/start-server.js
@@ -2,6 +2,7 @@ var debug       = require('debug')('serve')
 var connect     = require('connect')
 var jsx         = require('connect-jsx')
 var serveStatic = require('serve-static')
+var sass        = require('node-sass-middleware')
 
 
 var app = require('../app')
@@ -10,5 +11,9 @@ var app = require('../app')
 debug('Starting server at http://localhost:%s', app.port)
 var server = connect()
 server.use(jsx(app.root))
+server.use(sass({
+  src      : app.root,
+  response : true,
+}));
 server.use(serveStatic(app.root))
 server.listen(app.port)
